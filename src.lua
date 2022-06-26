fe0701a3226d8f2826b6f56f5cb7ebaeade06237
network <88440862+networktraffic@users.noreply.github.com>
2022-06-26 01:09:59 -0400

    remove useless space

--- src.lua
+++ src.lua
@@ -1,6 +1,5 @@
 return function( func, ... )
     local throwaway_thread = Instance.new( 'BindableEvent' )
-
     throwaway_thread.Event:Connect( func )
     throwaway_thread:Fire( ... )
     throwaway_thread:Destroy( )
