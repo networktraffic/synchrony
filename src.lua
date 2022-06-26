25ec51c210adbb3ee87b8504626b2d37494ccbeb
network <88440862+networktraffic@users.noreply.github.com>
2022-06-26 01:09:45 -0400

    remove useless function creation

--- src.lua
+++ src.lua
@@ -1,10 +1,7 @@
 return function( func, ... )
     local throwaway_thread = Instance.new( 'BindableEvent' )
 
-    throwaway_thread.Event:Connect( function( ... )
-        func( ... )
-    end )
-    
+    throwaway_thread.Event:Connect( func )
     throwaway_thread:Fire( ... )
     throwaway_thread:Destroy( )
 end
